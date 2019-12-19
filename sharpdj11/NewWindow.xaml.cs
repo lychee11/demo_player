913cda3cd7b8732ac1e39e0a37632d22590240ca
Vadim <vadner@gmail.com>
2019-12-19 22:46:34 +0300

    Third commit: GUI added

---
+++ sharpdj11/NewWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace sharpdj11
+{
+    /// <summary>
+    /// Interaction logic for NewWindow.xaml
+    /// </summary>
+    public partial class NewWindow : Window
+    {
+        public NewWindow()
+        {
+            InitializeComponent();
+        }
+        private void YB_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            this.Close();
+        }
+
+        private void NB_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            this.Close();
+        }
+    }
+}
